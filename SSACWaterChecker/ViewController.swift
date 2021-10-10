8c989944818134ca1b2ca64b7d6d6a5e0e0fbb35
sanghunlee <simoniful0gmail.com>
2021-10-11 00:00:15 +0900

    Initial Commit

---
+++ SSACWaterChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSACWaterChecker
+//
+//  Created by Sang hun Lee on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
