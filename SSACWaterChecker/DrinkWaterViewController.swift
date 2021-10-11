77438dab05bb83f6c21d39bed0207110709c8b51
sanghunlee <simoniful0gmail.com>
2021-10-11 12:00:33 +0900

    Add: additional function done and require refactoring

--- SSACWaterChecker/DrinkWaterViewController.swift
+++ SSACWaterChecker/DrinkWaterViewController.swift
@@ -30,6 +30,25 @@ class DrinkWaterViewController: UIViewController {
         let portion = Double(updatedNum) / (recommand * 1000) * 100
         portionIntake.text = "목표의 \(portion)%"
         
+        if 0..<10 ~= portion {
+            StatusImage.image = UIImage(named: "1-1")
+        } else if 10..<20 ~= portion {
+            StatusImage.image = UIImage(named: "1-2")
+        } else if 20..<30 ~= portion {
+            StatusImage.image = UIImage(named: "1-3")
+        } else if 30..<40 ~= portion {
+            StatusImage.image = UIImage(named: "1-4")
+        } else if 40..<50 ~= portion {
+            StatusImage.image = UIImage(named: "1-5")
+        } else if 50..<60 ~= portion {
+            StatusImage.image = UIImage(named: "1-6")
+        } else if 60..<70 ~= portion {
+            StatusImage.image = UIImage(named: "1-7")
+        } else if 70..<80 ~= portion {
+            StatusImage.image = UIImage(named: "1-8")
+        } else if 80..<100 ~= portion {
+            StatusImage.image = UIImage(named: "1-9")
+        }
     }
     
     
@@ -38,14 +57,49 @@ class DrinkWaterViewController: UIViewController {
         let intake: String? = intakeInput.text
         let value: Int = Int(intake!) ?? 0
         UserDefaults.standard.set(num + Int(value), forKey: "intake")
+        
         let updatedNum = UserDefaults.standard.integer(forKey: "intake")
         totalIntake.text = "\(updatedNum)ml"
         intakeInput.text = nil
+        
+        let height = UserDefaults.standard.string(forKey: "height") ?? "160.0"
+        let weight = UserDefaults.standard.string(forKey: "weight") ?? "50.0"
+        let recommand : Double = (Double(height)! + Double(weight)!) / 100
+        
+        let portion = Double(updatedNum) / (recommand * 1000) * 100
+        portionIntake.text = "목표의 \(portion)%"
+        
+        if 0..<10 ~= portion {
+            StatusImage.image = UIImage(named: "1-1")
+        } else if 10..<20 ~= portion {
+            StatusImage.image = UIImage(named: "1-2")
+        } else if 20..<30 ~= portion {
+            StatusImage.image = UIImage(named: "1-3")
+        } else if 30..<40 ~= portion {
+            StatusImage.image = UIImage(named: "1-4")
+        } else if 40..<50 ~= portion {
+            StatusImage.image = UIImage(named: "1-5")
+        } else if 50..<60 ~= portion {
+            StatusImage.image = UIImage(named: "1-6")
+        } else if 60..<70 ~= portion {
+            StatusImage.image = UIImage(named: "1-7")
+        } else if 70..<80 ~= portion {
+            StatusImage.image = UIImage(named: "1-8")
+        } else if 80..<100 ~= portion {
+            StatusImage.image = UIImage(named: "1-9")
+        }
     }
     
     @IBAction func handleRefresh(_ sender: UIButton) {
         UserDefaults.standard.set(0, forKey: "intake")
         let updatedNum = UserDefaults.standard.integer(forKey: "intake")
         totalIntake.text = "\(updatedNum)ml"
+        
+        let height = UserDefaults.standard.string(forKey: "height") ?? "160.0"
+        let weight = UserDefaults.standard.string(forKey: "weight") ?? "50.0"
+        let recommand : Double = (Double(height)! + Double(weight)!) / 100
+        let portion = Double(updatedNum) / (recommand * 1000) * 100
+        
+        portionIntake.text = "목표의 \(portion)%"
     }
 }
